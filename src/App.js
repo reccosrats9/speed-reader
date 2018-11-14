9a430cc7772f14e90ad8032232909f84267d5b65
Shannon Adair <shan@Shannons-MacBook-Pro.local>
2018-11-14 15:23:57 -0700

    little styling fixes

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
     this.setState({ displayBox: !this.state.displayBox })
   }
 
-  togglePause=()=> {
+  togglePause = () => {
     this.setState({ paused: !this.state.paused }, this.logger)
   }
 
@@ -46,7 +46,7 @@ class App extends Component {
         this.timer(i)
       } else if (i >= wordsArray.length) {
         this.toggleDisplay()
-        this.setState({ i: 0 })
+        this.setState({ i: 0, word: '' })
       }
     }
   }
@@ -62,28 +62,25 @@ class App extends Component {
       <div className="App">
         <h1 className={this.state.displayBox ? "title" : "small title"}>Speed Reader</h1>
         <div className='speedAndPause' >
-          <div className='customSelect'>
-        <select onChange={this.setSpeed} defaultValue='500' >
-          <option value="200">200 wpm</option>
-          <option value="300">300 wpm</option>
-          <option value="400">400 wpm</option>
-          <option value="450">450 wpm</option>
-              <option value="500">500 wpm</option>
-          <option value="550">550 wpm</option>
-          <option value="600">600 wpm</option>
-          <option value="650">650 wpm</option>
-          <option value="700">700 wpm</option>
-            </select>
-        {/* <span className='arrow' > ^ </span>     */}
-          </div>  
-        { !this.state.displayBox ?
-          this.state.paused ? 
+          <select onChange={this.setSpeed} defaultValue='500' >
+            <option value="200">200 wpm</option>
+            <option value="300">300 wpm</option>
+            <option value="400">400 wpm</option>
+            <option value="450">450 wpm</option>
+            <option value="500">500 wpm</option>
+            <option value="550">550 wpm</option>
+            <option value="600">600 wpm</option>
+            <option value="650">650 wpm</option>
+            <option value="700">700 wpm</option>
+          </select>
+          {!this.state.displayBox ?
+            this.state.paused ?
               <Play style={{ color: '#29F89C', height: 50, width: 50 }} onClick={this.togglePause} />
-              : <Pause style={{ color: '#29F89C', height: 50, width: 50 }} onClick={this.togglePause} />  
-        :null
+              : <Pause style={{ color: '#29F89C', height: 50, width: 50 }} onClick={this.togglePause} />
+            : null
           }
         </div>
-          
+
         <div className={this.state.displayBox ? "inputArea" : "hidden"}>
           <textarea placeholder="Paste your text and click the button to start speed reading" id="" cols="30" rows="10"
             onChange={this.updateInput} ></textarea>
