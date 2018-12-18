a2a036ef8a9146b2734c42f5e40b5f12b68293fd
Shannon Adair <shan@Shannons-MBP.attlocal.net>
2018-12-17 18:34:00 -0600

    added exit functionality and additional speeds

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Pause from '@material-ui/icons/PauseCircleFilled'
 import Play from '@material-ui/icons/PlayCircleFilled'
+import Close from '@material-ui/icons/Close'
 
 class App extends Component {
   constructor() {
@@ -29,6 +30,13 @@ class App extends Component {
     this.setState({ paused: !this.state.paused }, this.logger)
   }
 
+  exitPlayback = () => {
+    // this.toggleDisplay()
+    // this.togglePause()
+    // this.setState({i:0})
+    this.setState({ displayBox: true })
+  }
+
   setSpeed = (e) => {
     let speedInMS = 60000 / e.target.value
     this.setState({ speedInMS })
@@ -53,6 +61,9 @@ class App extends Component {
 
   timer = (i) => {
     setTimeout(() => {
+      this.state.displayBox ?
+        this.setState({ i: 0, word: '' })
+        :
       this.setState({ word: this.state.wordsArray[i], i: i + 1 }, this.logger)
     }, this.state.speedInMS)
   }
@@ -62,6 +73,7 @@ class App extends Component {
       <div className="App">
         <h1 className={this.state.displayBox ? "title" : "small title"}>Speed Reader</h1>
         <div className='speedAndPause' >
+          {!this.state.displayBox ? <Close style={{ color: '#29F89C', height: 40, width: 40}} onClick={this.exitPlayback} />  :null}  
           <select onChange={this.setSpeed} defaultValue='500' >
             <option value="200">200 wpm</option>
             <option value="300">300 wpm</option>
@@ -72,13 +84,19 @@ class App extends Component {
             <option value="600">600 wpm</option>
             <option value="650">650 wpm</option>
             <option value="700">700 wpm</option>
+            <option value="750">750 wpm</option>
+            <option value="800">800 wpm</option>
+            <option value="850">850 wpm</option>
+            <option value="900">900 wpm</option>
+            <option value="950">950 wpm</option>
+            <option value="1000">1000 wpm</option>
+            
           </select>
           {!this.state.displayBox ?
             this.state.paused ?
               <Play style={{ color: '#29F89C', height: 50, width: 50 }} onClick={this.togglePause} />
               : <Pause style={{ color: '#29F89C', height: 50, width: 50 }} onClick={this.togglePause} />
-            : null
-          }
+            : null}          
         </div>
 
         <div className={this.state.displayBox ? "inputArea" : "hidden"}>
